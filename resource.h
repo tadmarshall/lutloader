38532b13a17e74adf58db4e302491479241215f9
Tad Marshall <tad@tadmarshall.com>
2012-02-05 18:29:54 -0500

    Initial commit.
    LUT Loader is a program for viewing ICC color profiles and for
    loading color lookup tables (LUTs) into video cards.  LUTs are
    stored in ICC profiles in 'vcgt' (video card gamma table) tags.
    The program is Windows only (at this point) and early commits
    do almost nothing of interest.  Git commits are initially from
    occasional backups, so changes may be extreme and checkin
    comments haphazard or non-existent.

---
+++ resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by LUTloader.rc
+//
+#define IDS_CAPTION                     101
+#define IDD_SUMMARY_PAGE                107
+#define IDD_PROPPAGE_LARGE1             108
+#define IDD_OTHER_PAGE                  108
+#define IDC_SUMMARY_TEXT                1001
+#define IDC_OTHER_TEXT                  1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
